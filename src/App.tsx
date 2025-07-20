65b12f584a16ab090e43a856f161fdeb7187bb0b
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-07-20 19:08:52 +0000

    project initialized and set-up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className="">
+      <p className='bg-red-50 text-5xl'>Thus is shit man</p>
+    </main>
+  )
+}
+
+export default App
