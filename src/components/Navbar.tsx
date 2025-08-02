91880490827be8a3d4fdf08b0a1468575ce65fc3
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-08-02 15:48:47 +0000

    navbar component design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <div className=''>
+    <div className='flex w-full h-15 md:h-20 items-center justify-between'>
       {/* Logo */}
+      <div className="flex items-center gap-2">
+        <img src="/logo.png" alt="" className="w-8 h-8" />
+        <span className="text-2xl font-semibold text-[#222]">Safara</span>
+      </div>
 
       {/* Mobile Menu */}
+      <div className="md:hidden">
+        <div className="">
+          <img src={openMenu ? '/x.svg' : '/3-vert.svg'} alt="" className="h-8 w-8 object-cover cursor-pointer" onClick={() => setOpenMenu((prev) => !prev)}/>
+        </div>
+
+        {openMenu &&
+        <div className={`flex h-screen w-full flex-col items-center justify-center gap-5 text-lg text-[#000] font-[500] absolute top-15 ${openMenu ? '-right-0' : '-right-[100%]'} bg-[#51518d] transition-all ease-in-out`}>
+            <span className="">Home</span>
+            <span className="">Trending</span>
+            <span className="">Most Popular</span>
+            <span className="">About</span>
+
+            <div className="">
+              <button className="px-5 py-2 text-white bg-blue-700 rounded-3xl cursor-pointer">Login</button>
+            </div>
+        </div>}
+      </div>
 
       {/* Desktop Menu */}
+      <div className="hidden md:flex items-center">
+        <div className="flex items-center  gap-5 xl:gap-8 font-medium">
+          <span className="">Home</span>
+          <span className="">Trending</span>
+          <span className="">Most Popular</span>
+          <span className="">About</span>
+
+          <div className="">
+            <button className="px-5 py-2 text-white bg-blue-700 rounded-3xl cursor-pointer">Login</button>
+          </div>
+        </div>
+      </div> 
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar; 
