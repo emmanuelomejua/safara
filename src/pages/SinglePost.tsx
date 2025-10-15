b0ab34d9f8575c448e09f8f36c25a2d0c0337d2b
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-10-15 19:27:28 +0100

    implement single post page

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -1,8 +1,191 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import Image from "../components/ui/Image";
+
 
 const SinglePost = () => {
   return (
-    <div>SinglePost</div>
+    <div className='flex flex-col gap-8'>
+            {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-12">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse vero maxime enim
+          </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <span className="text-blue-800">Mazi Nwachi</span>
+            <span>on</span>
+            <span className="text-blue-800">Programming</span>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-gray-500 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse vero maxime enim, quae aut quo suscipit mollitia ipsum asperiores nulla hic et veniam! Quidem cum natus impedit nobis quae maxime?</p>
+        </div>
+          <div className="hidden lg:block w-2/5">
+            <Image src='/postImg.jpeg' alt="" className="rounded-2xl" />
+          </div>
+      </div>
+
+       <div className="flex flex-col md:flex-row gap-12 justify-between">
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+
+        </div>
+
+         {/* menu */}
+        <div className="px-4 h-max sticky top-8">
+           <h1 className="mb-4 text-sm font-medium">Author</h1>
+
+            <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+                <Image
+                  src='/postimg.jpeg'
+                  alt=""
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+              
+              <span className="text-blue-800">Emmanuel Nwachi</span>
+            </div>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur
+            </p>
+            <div className="flex gap-2">
+              <Link to='#'>
+                <Image alt="" src="/facebook.svg" />
+              </Link>
+              <Link to='#'>
+                <Image alt="" src="/instagram.svg" />
+              </Link>
+            </div>
+          </div>
+
+
+        </div>
+       </div>
+    </div>
   )
 }
 
